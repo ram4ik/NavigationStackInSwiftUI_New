fafd888213042c36ef642b6014728de6603b9a4d
Ramill <ramill_ibragimov@icloud.com>
2022-10-17 09:43:46 +0300

    Initial Commit

---
+++ NavigationStackInSwiftUI/NavigationStackInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackInSwiftUIApp.swift
+//  NavigationStackInSwiftUI
+//
+//  Created by Ramill Ibragimov on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
